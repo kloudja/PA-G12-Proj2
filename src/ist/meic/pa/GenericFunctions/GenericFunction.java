0ad8417b31aff0d509f3682504562f996dd1a9ec
rkd_s_000 <rkd_spkt@hotmail.com>
2016-04-28 11:53:09 +0100

    Initial commit

---
+++ src/ist/meic/pa/GenericFunctions/GenericFunction.java
@@ -0,0 +1,30 @@
+package ist.meic.pa.GenericFunctions;
+
+public class GenericFunction {
+	
+	String name;
+
+	public GenericFunction(String name){
+		
+		
+		this.name = name;
+		
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public void addMethod (GFMethod gfm){
+		
+	}
+	
+	public void addBeforeMethod (GFMethod gfm) {
+		
+	}
+	
+	public void addAfterMethod (GFMethod gfm) {
+		
+	}
+	
+}
